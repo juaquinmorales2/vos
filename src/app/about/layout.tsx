7e2340bff6a8ba8491f72619af2ec7409cae4dfd
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-03-10 16:37:47 +0500

    add framer motion && sass

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,13 +1,11 @@
-import "./globals.scss";
-
-export default function RootLayout({
+export default function AboutLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ color: "#f2f", padding: "20px" }}>{children}</body>
     </html>
   );
 }
---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+interface Props {}
+
+const About: FC<Props> = () => {
+  return <div>about</div>;
+};
+export default About;

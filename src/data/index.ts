f10b08f5c5460fa918c2beeac8cc16c9cf835b9b
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-05-09 22:53:23 +0500

    add book form input texts

--- src/data/index.ts
+++ src/data/index.ts
@@ -76,52 +76,70 @@ export const APPROACH_CARDS = [
 
 export const RADIO_FIELDS = [
   {
-    title: 'What service do you need?',
+    title: 'Какой тип услуг вас интересует?',
     classes: 'mr-8',
     radioArray: [
-      { name: 'Design / branding', value: '1' },
-      { name: 'Web development', value: '2' },
-      { name: 'Mobile Development', value: '3' },
-      { name: 'All of the above', value: '4' },
-      { name: 'Other', value: '44' },
+      { name: 'Дизайн / Брендинг', value: 'design/branding' },
+      { name: 'Веб разработка', value: 'web dev' },
+      { name: 'Мобильная разработка', value: 'mobile dev' },
+      { name: 'Все вышеперечисленные', value: 'design/branding/web/mobile' },
+      { name: 'Другое', value: 'other service' },
     ],
+    formKey: '_service',
   },
   {
-    title: 'What is your project budget?',
+    title: 'Каков ваш бюджет?',
     classes: '',
     radioArray: [
-      { name: '10000TMT - 20000TMT', value: '5' },
-      { name: '20000TMT - 30000TMT', value: '6' },
-      { name: '30000TMT - 50000TMT', value: '7' },
-      { name: '50000+', value: '8' },
+      { name: '10000TMT - 20000TMT', value: '10-20' },
+      { name: '20000TMT - 30000TMT', value: '20-30' },
+      { name: '30000TMT - 50000TMT', value: '30-50' },
+      { name: '50000TMT+', value: '50+' },
     ],
+    formKey: '_budget',
   },
   {
     title: 'Примерно, сколько страниц будет иметь ваш проект?',
     classes: 'mr-8',
     radioArray: [
-      { name: 'Меньше 5-ти', value: '9' },
-      { name: '6-10', value: '10' },
-      { name: '11-20', value: '11' },
-      { name: '20+', value: '12' },
+      { name: 'Меньше 5-ти', value: 'less than 5 page' },
+      { name: '6-10', value: '6-10 pages' },
+      { name: '11-20', value: '11-20 pages ' },
+      { name: '20+', value: '20+ pages' },
     ],
+    formKey: '_pages',
   },
   {
     title: 'Насколько быстро вам нужен проект?',
     classes: '',
     radioArray: [
-      { name: 'Максимально быстро', value: '13' },
-      { name: 'High prio (меньше 3х недель)', value: '14' },
-      { name: 'Regular', value: '15' },
-      { name: 'Take your time', value: '16' },
+      { name: 'Максимально быстро', value: 'maximum fast' },
+      { name: 'Высокая приоритетность ', value: 'high prio ' },
+      { name: 'Обычное время', value: 'regular' },
+      { name: 'Не торопитесь ', value: 'take your time' },
     ],
+    formKey: '_quickness',
   },
 ];
 
 export const INPUT_FIELDS = [
-  { label: 'first name', name: 'firstname', classes: 'inline-block !w-[calc(50%-16px)] mr-8' },
-  { label: 'last name', name: 'lastname', classes: 'inline-block !w-[calc(50%-16px)]' },
-  { label: 'email name', name: 'email', classes: '' },
-  { label: 'company name', name: 'companyname', classes: '' },
-  { label: 'website url', name: 'website', classes: '' },
+  { label: 'Выше имя', name: 'first', classes: 'inline-block !w-[calc(50%-16px)] mr-8', required: true },
+  { label: 'Ваш номер', name: 'last', classes: 'inline-block !w-[calc(50%-16px)]', type: 'number', required: true },
+  { label: 'Email', name: 'email', classes: '', type: 'email' },
+  { label: 'Название компании', name: 'company', classes: '', required: true },
+  { label: 'Ссылка на сайт', name: 'websiteUrl', classes: '' },
 ];
+
+export const BOOK_FORM_DEFAULT_STATE = {
+  _service: null,
+  _budget: null,
+  _pages: null,
+  _quickness: null,
+
+  first: '',
+  last: '',
+  email: '',
+  company: '',
+  websiteUrl: '',
+  message: '',
+};
--- src/widgets/BookForm/index.tsx
+++ src/widgets/BookForm/index.tsx
@@ -1,44 +1,46 @@
 'use client';
-import { FC, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FC, FormEvent, useState } from 'react';
 
+import { BOOK_FORM_DEFAULT_STATE, INPUT_FIELDS, RADIO_FIELDS } from '@/data';
+
+//components
 import Button from '@/components/ui/Button';
-import { INPUT_FIELDS, RADIO_FIELDS } from '@/data';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/RadioGroup';
 
 interface Props {}
 
-const defaultState = {
-  service: null,
-  budget: null,
-  pages: null,
-  quickness: null,
-
-  first: '',
-  last: '',
-  email: '',
-  company: '',
-  websiteUrl: '',
-  message: '',
-};
-
 const Index: FC<Props> = () => {
-  const [form, setForm] = useState(defaultState);
+  const [form, setForm] = useState(BOOK_FORM_DEFAULT_STATE);
 
+  const { push } = useRouter()
+
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('submit form', form)
+  }
   return (
     <div className="mx-auto max-w-[70vw] px-[10vw]">
-      <h1 className="mb-[1.25vw] text-center text-[3.5vw] font-bold leading-[100%]">hello world</h1>
-      <form className="flex h-full flex-col items-center">
+      <div className='relative'>
+        <button className=' p-[0.5vw] box-content rounded-full bg-stone-800 hover:bg-stone-800 group absolute z-10 top-[25%] left-0' onClick={() => push('/')}>
+          <svg focusable="false" className='w-[2vw] h-[2vw] fill-stone-400 group-hover:fill-stone-300 transition' viewBox="0 0 24 24" data-testid="ArrowBackOutlinedIcon" aria-label="fontSize medium"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"></path></svg>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} fill="#f2f" stroke="#22f"><path fill="#f2f" stroke="#22f" d="M244 400L100 256l144-144M120 256h292" /></svg> */}
+        </button>
+        <h1 className="mb-[1.75vw] text-center text-[3.5vw] font-bold leading-[100%]">Оставить заявку</h1>
+      </div>
+      <form className="flex h-full flex-col items-center" onSubmit={submit}>
         <div className="flex flex-wrap">
           {RADIO_FIELDS.map((item) => (
             <RadioGroup
-              onValueChange={(value) => console.log(value)}
+              onValueChange={(value) => setForm(prev => ({ ...prev, [item.formKey]: value }))}
               key={item.title}
               className={`mb-[1.75vw] inline-block w-[calc(50%-16px)] ${item.classes}`}
+              required={true}
             >
               <h4 className="mb-[0.2vw] max-w-[80%] text-[1.3vw] font-medium">{item.title}</h4>
               {item.radioArray.map((radio) => (
-                <div key={radio.value} className="flex items-center space-x-2 font-[400]">
-                  <RadioGroupItem value={radio.value} id={radio.name} />
+                <div key={radio.value} className="flex items-center space-x-[0.65vw] font-[400]">
+                  <RadioGroupItem value={radio.value} id={radio.name} required={true} />
                   <label htmlFor={radio.name} className="text-[1vw] leading-[1.75vw]">
                     {radio.name}
                   </label>
@@ -47,25 +49,32 @@ const Index: FC<Props> = () => {
             </RadioGroup>
           ))}
 
-          <div className="w-full space-y-6">
+          <div className="w-full space-y-6 text-[1.1vw]">
             {INPUT_FIELDS.map((item) => (
               <div key={item.label} className={`w-full ${item.classes}`}>
-                <label htmlFor={item.label} className="">
+                <label htmlFor={item.label} className="h-[4vw] ">
                   {item.label}
                 </label>
                 <input
-                  type="text"
+                  onChange={({ target: { name, value } }) => setForm(prev => ({ ...prev, [name]: value }))}
+                  type={item.type || 'text'}
+                  name={item.name}
                   id={item.label}
-                  className="w-full rounded-sm border border-primary/80 bg-transparent px-3 py-2"
+                  className="w-full rounded-sm border border-primary/80 bg-transparent px-[1vw] py-[0.8vw] h-[3vw] appearance-none"
+                  required={item.required}
                 />
               </div>
             ))}
-            <div className={`w-full `}>
-              <label htmlFor="message" className="">
-                message
+            <div className="w-full">
+              <label className="requiredd" htmlFor="message" >
+                Расскажите о вашем проекте
               </label>
               <textarea
+                minLength={20}
+                maxLength={500}
+                onChange={({ target: { name, value } }) => setForm(prev => ({ ...prev, [name]: value }))}
                 id="message"
+                name="message"
                 className="min-h-[10vw] w-full resize-none rounded-sm border border-primary/80 bg-transparent px-3 py-2"
               />
             </div>
@@ -73,6 +82,7 @@ const Index: FC<Props> = () => {
 
           <Button
             title="отправить"
+            type="submit"
             classes="py-[10px] px-12 text-[1.1vw] bg-bg-1/90 hover:bg-bg-1/80"
             btnClasses="p-[2px] capitalize self-start !mt-4"
           />

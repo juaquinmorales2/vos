90a8916d0402b5835255ea3407aa710439aa49ea
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-09-02 19:49:45 +0500

    add fluid cursor

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -2,20 +2,26 @@
 
 import { FC, useEffect } from 'react';
 
-import useShadowCursor from './useShadowCursor'
+import useShadowCursor from '@/shared/utils/useShadowCursor'
 
 interface Props { }
 
 const Index: FC<Props> = () => {
 
   useEffect(() => {
-    useShadowCursor()
+    useShadowCursor();
+
   }, [])
   return (
-    <section className='flex h-screen flex-col justify-center items-center w-full h-full m-0 p-0'>
-      <h1 className='absolute text-transparent bg-[conic-gradient(#ed0101,#3e2)] pointer-events-none mix-blend-difference bg-clip-text '>Move the mouse</h1>
-      <h2 className='absolute pointer-events-none tracking-[1.125px] text-[-webkit-xxx-large] font-black mix-blend-color-dodge mt-[25vh]'>See the magic</h2>
-      <canvas className='w-full h-full'></canvas>
+    <section className='flex flex-col justify-center items-center w-full h-full m-0 p-0'>
+
+      <div className='h-screen w-full fixed top-0 left-0 z-40 '>
+        <canvas id="fluid" className='w-full h-full' />
+      </div>
+
+      <div className='h-screen bg-slate-50 z-[100]'>123</div>
+      <div className='h-screen'>123</div>
+
     </section>
   );
 };
